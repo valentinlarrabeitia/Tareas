4f02e68ba3e2c21d3cb7b645c520587d28e50a4e
valentinlarrabeitia <valentin.larrabeitia@alumnos.ispi4038.edu.ar>
2024-09-04 23:58:30 -0300

    Add files via upload
    Agregado de funcionalidades

--- ventana_de_tareas.py
+++ ventana_de_tareas.py
@@ -8,3 +8,15 @@ class VentanaTareas(QtWidgets.QWidget):
         super().__init__()
         self.ventana = Ui_VentanaPrincipal()
         self.ventana.setupUi(self)
+        self.ventana.agregar_tarea.clicked.connect(self.agregar_tarea)
+        self.ventana.eliminar_tarea.clicked.connect(self.eliminar_tarea)
+        self.ventana.completar_tarea.clicked.connect(self.completar_tarea)
+
+    def agregar_tarea(self):
+        nueva_tarea = self.ventana.lineEdit.text()
+        self.ventana.listWidget.addItem(nueva_tarea)
+        self.ventana.lineEdit.clear()
+
+    def eliminar_tarea(self):
+        item_seleccionado = self.ventana.listWidget.currentRow()
+        self.ventana.listWidget.takeItem(item_seleccionado)
